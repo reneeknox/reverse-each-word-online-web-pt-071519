454edaa6d1171a8465a720694faa3904d76c91fa
Renee France <renee5.0@outlook.com>
2019-07-26 15:06:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
   string.split(" ").each do |word|
   reverse_each_word << word.reverse
   end
- reverse_each_word.join(" ")
- string.collect(" ") do |word|
- 
+ reverse_each_word.join(" ").each do |word|
+ string.collect do |word|
+   puts string
  end
 end
