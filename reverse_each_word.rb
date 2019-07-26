818a941c099ebf19a29bf45eeab7f4c5f7b62834
Renee France <renee5.0@outlook.com>
2019-07-26 13:59:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,8 @@ def reverse_each_word(string)
   string.split(" ").each do |word|
   reverse_each_word << word.reverse
   end
-  
-  reverse_each_word
+ reverse_each_word.join(" ")
+ string.collect(" ") do |word|
+ 
+ end
 end
